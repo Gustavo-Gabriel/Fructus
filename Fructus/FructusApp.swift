edc9ba37730be36f6ff7100aef9c213f1cc47816
Gustavo Anjos <gustavoanjos42@gmail.com>
2021-01-30 16:00:18 -0400

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Gustavo Anjos on 30/01/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
